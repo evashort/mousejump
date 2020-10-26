a9a9a3079780a29f04e39134794cf8a642f0ec31
Eva Short <evashort73@gmail.com>
2020-10-25 21:38:28 -0700

    drawParallelogram.py: c-friendly code

--- drawParallelogram.py
+++ drawParallelogram.py
@@ -1,36 +1,6 @@
 import imageio
 import numpy as np
 
-class Decider:
-    def __init__(self, left, right):
-        self.left = np.array(left, dtype=float)
-        self.normal = np.array(
-            [left[1] - right[1], right[0] - left[0]],
-            dtype=float
-        )
-        self.normal /= np.sqrt(np.sum(np.square(self.normal)))
-
-    def decide(self, p):
-        return np.sum((p - self.left) * self.normal)
-
-    def isXEntry(self):
-        return self.normal[0] > 0
-
-    def isXExit(self):
-        return self.normal[0] < 0
-
-    def getX(self, y):
-        return np.sum((self.left - [0, y]) * self.normal) / self.normal[0]
-
-    def intersect(self, other):
-        denominator = self.normal[1] * other.normal[0] \
-            - self.normal[0] * other.normal[1]
-        intermediate = (
-            other.normal * np.sum(self.left * self.normal) \
-                - self.normal * np.sum(other.left * other.normal)
-        ) / denominator
-        return np.array([-intermediate[1], intermediate[0]], dtype=float)
-
 class Point:
     pass
 
@@ -79,6 +49,7 @@ v1.y = -50
 v2 = Point()
 v2.x = -50
 v2.y = 150
+area = abs(v1.x * v2.y - v1.y * v2.x)
 n1 = getNormal(v1)
 n2 = scale(getNormal(v2), -1)
 n3 = scale(n1, -1)
@@ -88,62 +59,47 @@ p2 = add(origin, scale(n2, -0.5))
 p3 = add(add(origin, v2), scale(n3, -0.5))
 p4 = add(add(origin, v1), scale(n4, -0.5))
 
-#deciders = [Decider(p1, p2), Decider(p2, p3), Decider(p3, p4), Decider(p4, p1)]
 im = np.zeros((256, 256))
 im[10, 10] = 1
 for y in range(256):
     for x in range(256):
-        #im[y, x] = 1 - abs(min(decider.decide([x, y]) for decider in deciders))
         point = Point()
         point.x = x
         point.y = y
-        im[y, x] = 1 - abs(min(
-            dot(add(point, scale(p, -1)), n) - 0.5
+        im[y, x] = 0.4*(min(
+            dot(add(point, scale(p, -1)), n)
             for p, n in [(p1, n1), (p2, n2), (p3, n3), (p4, n4)]
-        ))
+        ) <= 0)
 
-# print(deciders[3].intersect(deciders[0]))
-# print(deciders[0].intersect(deciders[1]))
-# print(deciders[1].intersect(deciders[2]))
-# print(deciders[2].intersect(deciders[3]))
+class EdgePixel:
+    pass
 
-# for decider in deciders:
-#     decider.left -= 0.5 * decider.normal # inflate
+class Rib:
+    pass
 
-# yEntry = min(
-#     decider.intersect(deciders[(i+1) % len(deciders)])[1]
-#     for i, decider in enumerate(deciders)
-# )
-# yExit = max(
-#     decider.intersect(deciders[(i+1) % len(deciders)])[1]
-#     for i, decider in enumerate(deciders)
-# )
-
-yEntry = min(
+spine = Rib()
+spine.start = int(min(
     intersect(p1, n1, p2, n2).y,
     intersect(p2, n2, p3, n3).y,
     intersect(p3, n3, p4, n4).y,
     intersect(p4, n4, p1, n1).y
-)
-yExit = max(
+)) + 1
+spine.stop = int(max(
     intersect(p1, n1, p2, n2).y,
     intersect(p2, n2, p3, n3).y,
     intersect(p3, n3, p4, n4).y,
     intersect(p4, n4, p1, n1).y
-)
+)) + 1
 rowNormal = Point()
 rowNormal.x = 0
 rowNormal.y = 1
 rowPoint = Point()
-rowPoint.x = 0
-for y in range(int(yEntry), int(yExit + 2)):
-    # xEntry = max(
-    #     decider.getX(y) for decider in deciders if decider.isXEntry()
-    # )
-    # xExit = min(
-    #     decider.getX(y) for decider in deciders if decider.isXExit()
-    # )
+extraPixels = -round(area)
+edgePixels = []
+ribs = []
+for y in range(spine.start, spine.stop):
     rowPoint.y = y
+    rowPoint.x = 0
     xEntry = max(
         intersect(p, n, rowPoint, rowNormal).x
         for p, n in [(p1, n1), (p2, n2), (p3, n3), (p4, n4)]
@@ -154,6 +110,110 @@ for y in range(int(yEntry), int(yExit + 2)):
         for p, n in [(p1, n1), (p2, n2), (p3, n3), (p4, n4)]
         if n.x < 0
     )
-    im[y, :int(xEntry)] = 0.5
-    im[y, int(xExit + 2):] = 0.5
+    xCenter = int(0.5 * (xEntry + xExit)) + 1
+    rib = Rib()
+    rib.start = int(xEntry) + 1
+    rib.stop = int(xExit) + 1
+    ribs.append(rib)
+
+    extraPixels += rib.stop - rib.start
+    pixelPoint = Point()
+    point.y = y
+    for x in range(rib.start, xCenter):
+        rowPoint.x = x
+        distance = min(
+            dot(add(rowPoint, scale(p, -1)), n)
+            for p, n in [(p1, n1), (p2, n2), (p3, n3), (p4, n4)]
+        )
+        if distance >= 1:
+            break
+
+        im[y, x] = distance
+        edgePixel = EdgePixel()
+        edgePixel.y = y
+        edgePixel.right = False
+        edgePixel.value = distance
+        edgePixels.append(edgePixel)
+
+    for x in range(rib.stop - 1, xCenter - 1, -1):
+        rowPoint.x = x
+        distance = min(
+            dot(add(rowPoint, scale(p, -1)), n)
+            for p, n in [(p1, n1), (p2, n2), (p3, n3), (p4, n4)]
+        )
+        if distance >= 1:
+            break
+
+        im[y, x] = distance
+        edgePixel = EdgePixel()
+        edgePixel.y = y
+        edgePixel.right = True
+        edgePixel.value = distance
+        edgePixels.append(edgePixel)
+
+# https://en.wikipedia.org/wiki/Quickselect
+def partitionByValue(a, cutoffIndex, lowerCount, stop):
+    cutoff = a[cutoffIndex]
+    a[cutoffIndex] = a[stop - 1]
+    a[stop - 1] = cutoff
+
+    # a[:stop] is divided into 3 regions: lower, higher, and unknown
+    # or alternatively into 2 regions: known and unknown
+    # initially the "higher" region is empty
+    for knownCount in range(lowerCount, stop):
+        unknown = a[knownCount]
+        if le(unknown, cutoff):
+            # leapfrog to shift "higher" region right
+            a[knownCount] = a[lowerCount]
+
+            # move unknown into "lower" region
+            a[lowerCount] = unknown
+            lowerCount += 1
+
+    assert a[lowerCount - 1] == cutoff
+    return lowerCount
+
+import random
+def partitionByCount(a, desiredLowerCount, start=0, stop=None):
+    if stop is None:
+        stop = len(a)
+
+    if stop - start == 0:
+        return
+
+    cutoffIndex = random.randrange(start, stop)
+    lowerCount = partitionByValue(a, cutoffIndex, start, stop)
+    if desiredLowerCount < lowerCount:
+        partitionByCount(a, desiredLowerCount, start, lowerCount - 1)
+    elif desiredLowerCount > lowerCount:
+        partitionByCount(a, desiredLowerCount, lowerCount, stop)
+
+def le(a, b):
+    return a <= b
+
+for i in range(100):
+    a = [random.random() for i in range(20)]
+    sortedA = sorted(a)
+    k = random.randrange(21)
+    partitionByCount(a, k)
+    assert sorted(a) == sortedA
+    if k > 0 and k < len(a):
+        assert max(a[:k]) <= min(a[k:])
+
+def le(a, b): # pylint: disable=function-redefined
+    return (a.value, hash((a.right, a.y))) <= (b.value, hash((b.right, b.y)))
+
+partitionByCount(edgePixels, extraPixels)
+for i in range(extraPixels):
+    extraPixel = edgePixels[i]
+    ribIndex = extraPixel.y - spine.start
+    if extraPixel.right:
+        ribs[ribIndex].stop -= 1
+    else:
+        ribs[ribIndex].start += 1
+
+for i, rib in enumerate(ribs):
+    y = i + spine.start
+    im[y, rib.start:rib.stop] = 1
+
 imageio.imwrite('parallelogram.png', np.clip(im[::-1], 0, 1))
