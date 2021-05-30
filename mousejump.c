08d5ff5166cddf10d1458166ca9e9f40554ece95
Eva Short <evashort73@gmail.com>
2021-05-30 02:47:09 -0700

    fix build and launch configuration

--- mousejump.c
+++ mousejump.c
@@ -21,7 +21,7 @@
 #pragma comment(lib, "SHCore")
 #pragma comment(lib, "comctl32")
 // https://docs.microsoft.com/en-us/windows/win32/controls/cookbook-overview
-#pragma comment(linker,"\"/manifestdependency:type='win32' \
+#pragma comment(linker, "\"/manifestdependency:type='win32' \
 name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
 processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
