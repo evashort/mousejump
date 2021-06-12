fec7836d2fd7f29331a74c3874116f2258027258
Eva Short <evashort73@gmail.com>
2021-06-11 22:31:35 -0700

    add font picker control

--- MouseJumpSettings/Views/Appearance.xaml.cs
+++ MouseJumpSettings/Views/Appearance.xaml.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.InteropServices;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -11,9 +17,65 @@ namespace MouseJumpSettings.Views
     /// </summary>
     public sealed partial class Appearance : Page
     {
+        // https://docs.microsoft.com/en-us/windows/winui/api/microsoft.ui.xaml.controls.combobox?view=winui-3.0
+        ObservableCollection<ComboBoxItem> fonts = new ObservableCollection<ComboBoxItem>();
+
         public Appearance()
         {
             this.InitializeComponent();
+            LOGFONTW lf = new LOGFONTW();
+            lf.lfCharSet = 1;
+            lf.lfPitchAndFamily = 0;
+            lf.lfFaceName = "";
+            Dictionary<string, HashSet<int>> fontWeights = new Dictionary<string, HashSet<int>>();
+            EnumFontFamiliesExDelegate d = (ref LOGFONTW lplf, IntPtr lpntme, uint FontType, IntPtr lParam) =>
+            {
+            if (
+                // https://devblogs.microsoft.com/oldnewthing/20120719-00/?p=7093
+                !lplf.lfFaceName.StartsWith("@")
+                    && !lplf.lfFaceName.EndsWith(" Light")
+                    && !lplf.lfFaceName.EndsWith(" Light Condensed")
+                    && !lplf.lfFaceName.EndsWith(" Light SemiCondensed")
+                    && !lplf.lfFaceName.EndsWith(" Semilight")
+                    && !lplf.lfFaceName.EndsWith(" SemiLight")
+                    && !lplf.lfFaceName.EndsWith(" SemiLight Condensed")
+                    && !lplf.lfFaceName.EndsWith(" SemiLight SemiConde")
+                    && !lplf.lfFaceName.EndsWith(" Semibold")
+                    && !lplf.lfFaceName.EndsWith(" SemiBold")
+                    && !lplf.lfFaceName.EndsWith(" SemiBold Condensed")
+                    && !lplf.lfFaceName.EndsWith(" SemiBold SemiConden")
+                    && !lplf.lfFaceName.EndsWith(" Medium")
+                    && !lplf.lfFaceName.EndsWith(" Black")
+                    && !lplf.lfFaceName.EndsWith(" MDL2 Assets")
+                    && lplf.lfFaceName != "OpenSymbol"
+                )
+                {
+                    HashSet<int> weights;
+                    if (!fontWeights.TryGetValue(lplf.lfFaceName, out weights))
+                    {
+                        weights = new HashSet<int>();
+                        fontWeights[lplf.lfFaceName] = weights;
+                    }
+
+                    weights.Add(lplf.lfWeight);
+                }
+
+                return 1;
+            };
+
+            IntPtr hdc = Win32.GetDC(IntPtr.Zero);
+            var r = Win32.EnumFontFamiliesExW(hdc, ref lf, d, IntPtr.Zero, 0);
+            Win32.ReleaseDC(IntPtr.Zero, hdc);
+            foreach (KeyValuePair<string, HashSet<int>> pair in fontWeights.OrderBy(pair => pair.Key))
+            {
+                fonts.Add(
+                    new ComboBoxItem {
+                        Content = pair.Key,
+                        FontFamily = new FontFamily(pair.Key),
+                        FontWeight = new Windows.UI.Text.FontWeight((ushort)pair.Value.Min())
+                    }
+                );
+            }
         }
 
         private void ColorPicker_Loaded(object sender, RoutedEventArgs e)
---
+++ MouseJumpSettings/Win32.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MouseJumpSettings
+{
+    // https://social.msdn.microsoft.com/Forums/vstudio/en-US/9beda373-b616-43e7-8eca-f2cc876385db/how-can-i-get-the-language-of-the-fontfamily
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct LOGFONTW
+    {
+        public int lfHeight;
+        public int lfWidth;
+        public int lfEscapement;
+        public int lfOrientation;
+        public int lfWeight;
+        public byte lfItalic;
+        public byte lfUnderline;
+        public byte lfStrikeOut;
+        public byte lfCharSet;
+        public byte lfOutPrecision;
+        public byte lfClipPrecision;
+        public byte lfQuality;
+        public byte lfPitchAndFamily;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string lfFaceName;
+    }
+
+    public delegate int EnumFontFamiliesExDelegate(ref LOGFONTW lplf, IntPtr lpntme, uint FontType, IntPtr lParam);
+
+    public class Win32
+    {
+        [DllImport("gdi32", CharSet = CharSet.Unicode)]
+        public static extern int EnumFontFamiliesExW(
+            IntPtr hdc,
+            ref MouseJumpSettings.LOGFONTW lpLogfont,
+            MouseJumpSettings.EnumFontFamiliesExDelegate
+            lpEnumFontFamExProc,
+            IntPtr lParam,
+            uint dwFlags
+        );
+
+        [DllImport("user32")]
+        public static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("user32")]
+        public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hdc);
+    }
+}
