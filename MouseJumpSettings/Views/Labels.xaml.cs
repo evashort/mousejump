e22db79b52786f1b9fe932ad292cc142bb45a470
Eva Short <evashort73@gmail.com>
2022-02-25 16:27:01 -0800

    make tree view for labels in MouseJumpSettings

--- MouseJumpSettings/Views/Labels.xaml.cs
+++ MouseJumpSettings/Views/Labels.xaml.cs
@@ -1,18 +1,57 @@
 ﻿using Microsoft.UI.Xaml.Controls;
+using System.Collections.ObjectModel;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace MouseJumpSettings.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class Labels : Page
     {
+        // https://docs.microsoft.com/en-us/windows/apps/design/controls/tree-view#tree-view-using-data-binding
+        private ObservableCollection<Item> DataSource = new ObservableCollection<Item>();
+
         public Labels()
         {
             this.InitializeComponent();
+            DataSource = GetData();
+        }
+
+        private ObservableCollection<Item> GetData()
+        {
+            return new ObservableCollection<Item>
+            {
+                new Item
+                {
+                    Name = "main",
+                    Children = new ObservableCollection<Item>
+                    {
+                        new Item
+                        {
+                            Name = "letters",
+                        },
+                        new Item
+                        {
+                            Name = "numbers",
+                        },
+                    },
+                },
+                new Item
+                {
+                    Name = "unused",
+                },
+            };
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public ObservableCollection<Item> Children { get; set; } = new ObservableCollection<Item>();
+
+        public override string ToString()
+        {
+            return Name;
         }
     }
 }
