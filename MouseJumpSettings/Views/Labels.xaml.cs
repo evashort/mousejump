690e485ead7fabf333db0da1048b8eeea65bea42
Eva Short <evashort73@gmail.com>
2022-02-25 20:57:53 -0800

    prototype labels tree view

--- MouseJumpSettings/Views/Labels.xaml.cs
+++ MouseJumpSettings/Views/Labels.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using System.Collections.ObjectModel;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -24,29 +25,113 @@ namespace MouseJumpSettings.Views
                 new Item
                 {
                     Name = "main",
+                    Operation = Operation.Union,
                     Children = new ObservableCollection<Item>
                     {
                         new Item
                         {
                             Name = "letters",
+                            Operation = Operation.Literal,
+                            Weight = 1,
+                        },
+                        new Item
+                        {
+                            IsSeparator = true,
                         },
                         new Item
                         {
                             Name = "numbers",
+                            Operation = Operation.Difference,
+                            Weight = 2,
                         },
                     },
                 },
                 new Item
                 {
                     Name = "unused",
+                    Operation = Operation.Join,
                 },
             };
         }
     }
 
+    public enum Operation
+    {
+        Literal,
+        Union,
+        Join,
+        Difference,
+    }
+
     public class Item
     {
         public string Name { get; set; }
+        public Operation Operation { get; set; }
+        public string Icon
+        {
+            get
+            {
+                // https://docs.microsoft.com/en-us/windows/apps/design/style/segoe-ui-symbol-font
+                switch (Operation)
+                {
+                    case Operation.Literal:
+                        return "\xEA37";
+                    case Operation.Union:
+                        return "\xE948";
+                    case Operation.Join:
+                        return "\xE947";
+                    case Operation.Difference:
+                        return "\xE71C";
+                }
+
+                return "";
+            }
+        }
+        public double? Weight { get; set; }
+        public bool Used { get; set; }
+        public bool IsSeparator { get; set; }
+        public string AltText {
+            get
+            {
+                if (IsSeparator)
+                {
+                    return "Separator";
+                }
+
+                string OperationText = "";
+                switch (Operation)
+                {
+                    case Operation.Literal:
+                        OperationText = "List";
+                        break;
+                    case Operation.Union:
+                        OperationText = "Union";
+                        break;
+                    case Operation.Join:
+                        OperationText = "Join";
+                        break;
+                    case Operation.Difference:
+                        OperationText = "Filter";
+                        break;
+                }
+
+                if (Weight != null)
+                {
+                    return $"{Name}, {OperationText}, {Weight}";
+                }
+                else
+                {
+                    return $"{Name}, {OperationText}";
+                }
+            }
+        }
+        public Visibility SeparatorVisibility {
+            get { return IsSeparator ? Visibility.Visible : Visibility.Collapsed; }
+        }
+        public Visibility NameVisibility
+        {
+            get { return IsSeparator ? Visibility.Collapsed : Visibility.Visible; }
+        }
         public ObservableCollection<Item> Children { get; set; } = new ObservableCollection<Item>();
 
         public override string ToString()
